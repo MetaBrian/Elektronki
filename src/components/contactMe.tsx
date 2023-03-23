39e6d929d4a475331227565e01eb0baebe1ef984
MetaBrian <114273788+MetaBrian@users.noreply.github.com>
2023-03-23 18:15:43 -0400

    added additional external linking buttons

--- src/components/contactMe.tsx
+++ src/components/contactMe.tsx
@@ -9,7 +9,12 @@ function contactMe() {
                     <p>All rights reserved</p>
                 </span>
             </div>
-            <button className='navButtons'>Contact me</button>
+            <button className='navButtons'>
+                Contact me
+            </button>
+            <button className='navButtons'>
+                <img height='10vh' width='50vw' src='../src/assets/elektronki.png' alt='Github Logo'/>
+            </button>
         </div>
         </>
     )
