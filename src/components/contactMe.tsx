5afb8186c35e5fc913bce379adb776d7341ccf04
MetaBrian <114273788+MetaBrian@users.noreply.github.com>
2023-03-24 17:14:35 -0400

    integrated base design

--- src/components/contactMe.tsx
+++ src/components/contactMe.tsx
@@ -9,12 +9,15 @@ function contactMe() {
                     <p>All rights reserved</p>
                 </span>
             </div>
+        <div>
+            Questions or Concerns? Contact me via Github or LinkedIn
             <button className='navButtons'>
-                Contact me
+                <img height='100vh' width='100vw' src='../src/assets/linkedinicon.png' alt='linkedin icon'/>
             </button>
             <button className='navButtons'>
-                <img height='10vh' width='50vw' src='../src/assets/elektronki.png' alt='Github Logo'/>
+                <img height='100vh' width='100vw' src='../src/assets/GitHub-logo.png' alt='Github Logo'/>
             </button>
+            </div>
         </div>
         </>
     )
--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -6,23 +6,37 @@ function NavBar() {
         <>
         <div className="navBar">
 
-            <img id='elektronkiLogo' src='../src/assets/elektronki.png' alt="Elektronki's logo"/>
-            <Link to='/error'>Error page</Link>
             
+            <Link to='/error'>
+            <img id='elektronkiLogo' src='../src/assets/elektronki.png' alt="Elektronki's logo"/>
+                Elektronki
+            </Link>
+            
+         
+
+          
+{/* 
+                make below into links */}
+            <button className='navButtons'>
+                Flashcards
+            </button>
+
+            <button className='navButtons'>
+                Create a card
+            </button>
+
+            <button className='navButtons'>
+                Exam Mode
+            </button>
+{/* 
+            below buttons remain buttons */}
+
             <button className='navButtons'onClick= {() => {window.open('https://github.com/MetaBrian/Elektronki','_blank')}}>
-                Github
+                <img height= '100vh' width='100vw'src='../src/assets/GitHub-logo.png' alt='Github logo button leading to the project"s repo'/>
             </button>
 
             <button className='navButtons'>
-                Quiz Mode
-            </button>
-
-            <button className='navButtons'>
-                My FlashCards
-            </button>
-
-            <button className='navButtons'>
-                Mode Switch
+                <img height='50vh' width= '100vw'src="../src/assets/light&dark-icon.png" alt='light and dark icon'/>
             </button>
             
         </div>
