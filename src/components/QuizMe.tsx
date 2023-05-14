010ece86d349652b75b9d1822f966bcb33b891f1
MetaBrian <114273788+MetaBrian@users.noreply.github.com>
2023-05-14 18:12:36 -0400

    updated upcoming quizme feature

--- src/components/QuizMe.tsx
+++ src/components/QuizMe.tsx
@@ -7,21 +7,10 @@ function ExamMode(){
             <h2 className='PageTitle'>Choose a Topic</h2>
        
 
-            <div className='myFlashCards'>
-            <button className='FlashCardsButtons' onClick={()=>{console.log('clickedfolder1')}}>
-                <img height='50%' width='50%' src='../src/assets/black-folder.png' alt="a black folder"/>
-                <p>Folder 1 (20)</p>
-            </button>
+            <div className='QuizMe'>
+         
+            <h3>Coming Soon...</h3>
 
-            <button className='FlashCardsButtons' onClick={()=>{console.log('clickedfolder2')}}>
-                <img height='50%' width='50%' src='../src/assets/black-folder.png' alt="a black folder"/>
-                Folder 2 (79)
-            </button>
-
-            <button className='FlashCardsButtons' onClick={()=>{console.log('clickedfoldertotal')}}>
-                <img height='50%' width='50%' src='../src/assets/black-folder.png' alt="a black folder"/>
-                All Cards (99)
-            </button>
             </div>
             
         </div>
